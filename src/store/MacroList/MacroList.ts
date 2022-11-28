4b3e381052fd5903a89df9c46851fae13805a173
Dinar <82703414+DinarSharipov@users.noreply.github.com>
2022-11-28 22:54:11 +0300

    Cra test (#13)
    * preparing
    
    * preparing
    
    * preparing
    
    * билд настроен
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    * process
    
    Co-authored-by: Dinar <pravovoy116@gmail.com>

--- src/store/MacroList/MacroList.ts
+++ src/store/MacroList/MacroList.ts
@@ -4,6 +4,8 @@ import { MacroInfo } from './MacroListTypes';
 class MacroList {
   entities?: MacroInfo[];
 
+  state: 
+
   constructor() {
     makeAutoObservable(this);
     this.entities = [
@@ -11,6 +13,14 @@ class MacroList {
       { id: '2m', name: 'Второй макрос' },
       { id: '3m', name: 'Третий макрос' },
     ];
+  }
+
+  public fetch() {
+    try {
+      console.log('fetch');
+    } catch (error) {
+      console.warn(error);
+    }
   }
 }
 
