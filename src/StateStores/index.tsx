ecf4ff32782d2284814758df0344c5d6a3ed8646
Dinar Sharipov <Donotcoverr@mail.ru>
2022-12-04 23:49:54 +0300

    completed

--- src/StateStores/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ErrorState } from './ErrorState';
-export { default as SuccessState } from './SuccessState';
-export { default as FetchingState } from './FetchingState';
-export { default as BaseState } from './BaseState';
