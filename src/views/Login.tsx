9a4e585ec89f394500e2542e14302f69a3f749d8
Dinar <pravovoy116@gmail.com>
2022-12-22 21:01:16 +0300

    completed

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import { Button } from '../components';
 import Input from '../components/ui/Input/Input';
 
-const Login: React.FC = () => (
-  <div className="flex flex-1 flex-col justify-center w-full  h-full items-center">
-    <form  className="w-1/2 min-w-[300px] max-w-[400px]
+const Login: React.FC = () => {
+  const formChangeHandler = (event?: FormEvent<HTMLFormElement>) => {
+    const { value, id } = event?.target as HTMLInputElement;
+    console.log(value, id);
+  };
+
+  return (
+    <div className="flex flex-1 flex-col justify-center w-full  h-full items-center">
+      <form
+        className="w-1/2 min-w-[300px] max-w-[400px]
         shadow-lg
         rounded-main
         mt-[20vh]
@@ -13,12 +20,22 @@ const Login: React.FC = () => (
         self-center
         justify-self-center
         text-center"
-    >
-      <Input label='Имя пользователя' placeholder="Введите имя пользователя" />
-      <Input label='Пароль' placeholder="Введите пароль" />
-    </form>
-    <Button.Primary onClick={() => console.log(1)}>Войти</Button.Primary>
-  </div>
-);
+        onChange={formChangeHandler}
+      >
+        <Input
+          id="login"
+          label="Имя пользователя"
+          placeholder="Введите имя пользователя"
+        />
+        <Input
+          id="password"
+          label="Пароль"
+          placeholder="Введите пароль"
+        />
+      </form>
+      <Button.Primary onClick={() => console.log(1)}>Войти</Button.Primary>
+    </div>
+  );
+};
 
 export default React.memo(Login);
